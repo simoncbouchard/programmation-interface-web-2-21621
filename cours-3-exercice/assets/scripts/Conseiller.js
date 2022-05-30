b52c1db36a29e4bc099a647d6eb3df82094bdb32
simoncbouchard <simoncbouchard@gmail.com>
2022-05-30 16:32:25 -0400

    Add cours-3-exercice

---
+++ cours-3-exercice/assets/scripts/Conseiller.js
@@ -0,0 +1,8 @@
+/**
+ * On définit notre espace de nom 
+ * qui est en fait une variable globale qui permettra de protéger notre code et celui des autres.
+ * L'instruction suivante affecte la variable global MON_ESPACE_NOM à deux options 
+ * (le || permet de distinguer les deux options).
+ * Option 1 : Si la variable globale MON_ESPACE_NOM existe déjà on récupère son contenu et l'affecte à l'objet MON_ESPACE_NOM
+ * Option 2 : Si la variable globale MON_ESPACE_NOM n'existe pas on l'affecte à l'objet vide {} 
+ */
---
+++ cours-3-exercice/assets/scripts/main.js
@@ -0,0 +1,15 @@
+(function(MEN) {
+
+    /**
+     * Récupère les objets stockés dans l'objet MON_ESPACE_NOM (MEN)
+     */
+    let Personne = MEN.Personne,
+        Etudiant = MEN.Etudiant,
+        Professeur = MEN.Professeur
+
+
+
+
+
+
+})(MON_ESPACE_NOM);
