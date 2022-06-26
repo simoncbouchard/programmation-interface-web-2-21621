b136ba3bf033a3d0654fd9cc121d8db8a358728e
simoncbouchard <simoncbouchard@gmail.com>
2022-06-26 12:24:42 -0400

    Add cours-6-exercice-solutionnaire (méthode)

---
+++ cours-6-exercice-solutionnaire-export-injectText/assets/scripts/classeMapping.js
@@ -0,0 +1,10 @@
+import Cupcake from './Cupcake.js';
+import Hipster from './Hipster.js';
+import Samuel from './Samuel.js';
+
+
+export const classesMapping = {
+    'Cupcake': Cupcake,
+    'Hipster': Hipster,
+    'Samuel': Samuel
+};
