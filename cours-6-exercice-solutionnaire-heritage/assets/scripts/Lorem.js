8b163ada3e422eb4e7a3bbeafda82083d3715b4e
simoncbouchard <simoncbouchard@gmail.com>
2022-06-26 12:25:11 -0400

    Add cours-6-exercice-solutionnaire (héritage)

---
+++ cours-6-exercice-solutionnaire-heritage/assets/scripts/Lorem.js
@@ -0,0 +1,13 @@
+export default class Lorem {
+    injectText(el, textArray) {
+
+        let arrayLength = textArray.length,
+            randomNumber = Math.floor(Math.random() * arrayLength),
+            dataset = el.dataset.jsComponent;
+
+        el.innerHTML = `
+                        <h3>${dataset}</h3>
+                        <p>${textArray[randomNumber]}</p>
+                        `;
+    }
+}
